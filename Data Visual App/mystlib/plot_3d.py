1afbd61e226f7b61d636cf042a818947edbb0bc5
Sharky1507 <101697894+Sharky1507@users.noreply.github.com>
2024-05-01 21:23:09 +0530

    Add files via upload

---
+++ Data Visual App/mystlib/plot_3d.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import streamlit as st
+import pydeck as pdk
+def run(df):
+    st.subheader('Plot 01: ')
+    fig1 = px.scatter_3d(df,
+    x = 'passenger_count',
+    y = 'distance',
+    z= 'duration',
+    title= "Relation between Passenger, Distance and Duration",
+    color = 'passenger_count',
+    width = 800,
+    height = 600,
+
+
+    )
+    st.plotly_chart(fig1)
+    #plot no 2
+    st.subheader("Plot no 2")
+    df = df.explode(['polyline_lat','polyline_lon'], ignore_index = True)
+    view = pdk.data_utils.compute_view(df[['polyline_lon','polyline_lat']])
+    view.pitch = 70
+    view.bearing = 60
+    view.latitude = df['polyline_lat'].iloc[-1]
+    view.longitude = df['polyline_lon'].iloc[-1]
+    view.zoom = 8
+    view.min_zoom = 3
+    view.max_zoom = 30
+
+    layer = pdk.Layer(
+        'HexagonLayer',
+        df,
+        get_position = ['polyline_lon','polyline_lat'],
+        auto_highlight = True,
+        elevation_scale = 50,
+        get_radius = 5,
+        pickable=True,
+        extruded = True,
+        elevation_range = [0,3000],
+        coverage = 1,
+        getTextAnchor = '"middle"',
+        get_alignment_baseline = '"bottom"'
+
+    )
+    tooltip = {
+        "html": "No. of Rides take from here: {elevationValue}"
+    }
+    final = pdk.Deck(
+        layers = [layer],
+        initial_view_state = view,
+        tooltip = tooltip
+
+    )
+    st.pydeck_chart(final)
+
---
+++ Data Visual App/mystlib/viewonMap.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+def run(df):
+    #Enter your MapBox token here
+    px.set_mapbox_access_token("")
+    st.subheader("Plot 1")
+    fig1 = px.scatter_mapbox(
+        df,
+        lat = "pickup_latitude",
+        lon = "pickup_longitude",
+        size = "passenger_count",
+        color = "passenger_count",
+        center = dict(
+            lat = float(df["pickup_latitude"][0]),
+            lon =float(df["pickup_longitude"][0]) 
+
+        ),
+        zoom= 10, width = 800 , height = 600
+
+
+    )
+    fig1.update_layout(title_text = 'Passengers in Taxi', title_x = 0.5, title_y = 1)
+    st.plotly_chart(fig1,use_container_width = True)
+    # second plot
+    st.subheader("Plot 2: ")
+    st.write("Select trip Id to Visualize")
+    option = st.selectbox(' ', df["ID"])
+    trip = df[df["ID"] == option]
+    trip['lat'] = df['polyline_lat']
+    trip['lon'] = df['polyline_lon']
+    trip = trip.explode(['lat','lon'],ignore_index = True)
+    fig2 = px.line_mapbox(trip,
+        lat = 'lat', lon = 'lon', zoom = 10, width = 800 , height = 600
+    
+    )
+    fig2.update_layout(title_text = "Trip Routes", title_x = 0.5, title_y = 1)
+    st.plotly_chart(fig2,use_container_width=True)
